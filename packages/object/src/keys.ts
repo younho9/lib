9574c2f7e3bcb335f047b3caa329f1072b47e470
Younho Choo <younho9.choo@gmail.com>
2021-09-21 23:28:50 +0900

    feat(`object`): use type casting

--- packages/object/src/keys.ts
+++ packages/object/src/keys.ts
@@ -1,32 +1,38 @@
 import type {Alt} from '@younho9/types';
 
+export type ObjectKeys<T> = T extends Alt.Object
+  ? keyof T extends symbol
+    ? []
+    : Exclude<keyof T, symbol>[]
+  : T extends number | bigint
+  ? []
+  : T extends Array<unknown> | string
+  ? string[]
+  : never;
+
 /**
- * Using declaration merging feature
+ * Option 1. Type casting
+ * Option 2. Extending Object Constructor using merging feature.
  *
  * @see https://fettblog.eu/typescript-better-object-keys/#option-2.-extending-object-constructor
- */
-declare global {
-  export type ObjectKeys<T> = T extends Alt.Object
-    ? keyof T extends symbol
-      ? []
-      : Exclude<keyof T, symbol>[]
-    : T extends number | bigint
-    ? []
-    : T extends Array<unknown> | string
-    ? string[]
-    : never;
-
-  export interface ObjectConstructor {
-    keys<T>(o: T): ObjectKeys<T>;
-  }
-}
-
-/**
- * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+ *
+ * @example - Option2
+ * ```ts
+ * declare global {
+ *   export interface ObjectConstructor {
+ *     keys<T>(o: T): ObjectKeys<T>;
+ *   }
+ * }
+ * ```
+ *
+ * Option 2 can pollute the global type, so use Option 1.
+ * @see https://github.com/microsoft/TypeScript/pull/12253#issuecomment-263132208
  *
  * @param o - The object of which the enumerable's own properties are to be returned.
  * @returns An array of strings that represent all the enumerable properties of the given object.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
  */
 export default function keys<T>(o: T): ObjectKeys<T> {
-  return Object.keys(o);
+  return Object.keys(o) as ObjectKeys<T>;
 }
