0e77cdec7771747abc11ad585295866d1ec15654
Younho Choo <younho9.choo@gmail.com>
2021-09-21 22:14:49 +0900

    chore(`jest`): ignore `test-d.ts` files in jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
+  testPathIgnorePatterns: ['\\.test-d\\.ts'],
 };
 
 export default config;
