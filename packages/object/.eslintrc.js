7ce9878bb0080e3e8b0baf88eed2bffcfe5c9e3d
Younho Choo <younho9.choo@gmail.com>
2021-09-25 10:14:31 +0900

    chore(`eslint`): ignore `object` & `Function` type of ban-types

--- packages/object/.eslintrc.js
+++ packages/object/.eslintrc.js
@@ -1,14 +1,12 @@
-const {OFF, ERROR} = require('@younho9/eslint-plugin/constants');
+const {ERROR} = require('@younho9/eslint-plugin/constants');
 
 module.exports = {
   rules: {
-    '@typescript-eslint/no-explicit-any': OFF,
     '@typescript-eslint/ban-types': [
       ERROR,
       {
         types: {
           object: false,
-          Function: false,
         },
         extendDefaults: true,
       },
