d1394f73b55674d87ae14f518b6ae33e7289d666
Younho Choo <younho9.choo@gmail.com>
2021-10-05 17:06:30 +0900

    docs(`eslint-plugin`): add installation & usage

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,31 +2,17 @@
 module.exports = {
   root: true,
   extends: ['./packages/eslint-plugin/dist/configs/recommended'],
+  /** @see https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration */
   parserOptions: {
     project: ['./tsconfig.json', './packages/*/tsconfig.json'],
     ecmaVersion: 'latest',
   },
-  overrides: [
-    {
-      files: ['*.jsx'],
-      extends: ['./packages/eslint-plugin/dist/configs/react'],
-    },
-    {
-      files: ['*.ts'],
-      extends: ['./packages/eslint-plugin/dist/configs/typescript'],
-    },
-    {
-      files: ['*.tsx'],
-      extends: [
-        './packages/eslint-plugin/dist/configs/react',
-        './packages/eslint-plugin/dist/configs/typescript',
-      ],
-    },
-  ],
   settings: {
+    /** @see https://github.com/import-js/eslint-plugin-import#importparsers */
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
+    /** @see  https://github.com/alexgorbatchev/eslint-import-resolver-typescript#configuration */
     'import/resolver': {
       typescript: {
         project: 'packages/*/tsconfig.json',
--- packages/eslint-plugin/README.md
+++ packages/eslint-plugin/README.md
@@ -2,6 +2,41 @@
 
 > Personal ESLint plugin with multiple configs
 
+## Installation
+
+Install `@younho9/eslint-plugin` with peerDependencies:
+
+```sh
+npx install-peerdeps --dev @younho9/eslint-plugin
+```
+
+## Usage
+
+`.eslintrc.js` or `.eslintrc.cjs`
+
+<!-- prettier-ignore-start -->
+```js
+/** @type {import("@types/eslint").Linter.Config } */
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:@younho9/recommended',
+  ],
+  /** @see https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration */
+  parserOptions: {
+    project: ['./tsconfig.json', './packages/*/tsconfig.json'],
+    ecmaVersion: 'latest',
+  },
+  settings: {
+    /** @see https://github.com/import-js/eslint-plugin-import#importparsers */
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
+};
+```
+<!-- prettier-ignore-end -->
+
 ## Configs
 
 ### [recommended](src/configs/recommended.ts)
