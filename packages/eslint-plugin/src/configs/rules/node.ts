75199526b9bfec7265d8fa81cc6c3f1e3453eb05
Younho Choo <yo+github@younho9.dev>
2021-10-07 10:17:41 +0900

    feat(`eslint-plugin`): off eslint-plugin-node rules with bugs

--- packages/eslint-plugin/src/configs/rules/node.ts
+++ packages/eslint-plugin/src/configs/rules/node.ts
@@ -1,7 +1,10 @@
 import type {Linter} from 'eslint';
 
-import {WARN} from '../../constants/index.js';
+import {OFF} from '../../constants/index.js';
 
 export default {
-  'node/no-missing-import': WARN,
+  'node/no-missing-import': OFF,
+  'node/no-missing-require': OFF,
+  'node/no-extraneous-import': OFF,
+  'node/no-extraneous-require': OFF,
 } as Linter.RulesRecord;
