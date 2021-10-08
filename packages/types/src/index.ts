ee1cb1c330637329b2d685621182a1bae3cf308e
Younho Choo <yo+github@younho9.dev>
2021-10-08 12:38:46 +0900

    build(`types`): support esm

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,5 @@
-export * from './basic';
-export * as Alt from './alternative';
-export * from './convenience';
-export * from './invariantOf';
-export * from './object';
+export * from './basic.js';
+export * as Alt from './alternative.js';
+export * from './convenience.js';
+export * from './invariantOf.js';
+export * from './object.js';
