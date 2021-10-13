ac0f0588e78380e4ba924c79ee838b1250c95174
Younho Choo <yo+github@younho9.com>
2021-10-13 16:38:58 +0900

    fix(`types`): remove unused class

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,9 +2,3 @@ export * from './basic.js';
 export * as Alt from './alternative.js';
 export * from './convenience.js';
 export * from './object.js';
-
-export class Shape {
-  constructor() {
-    this.__keys__ = undefined;
-  }
-}
