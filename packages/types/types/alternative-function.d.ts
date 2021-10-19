19af873d1104a2c7f801e0d42a9673c0b7e2b1a1
Younho Choo <yo+github@younho9.com>
2021-10-20 02:03:32 +0900

    docs(`types`): move README.md to doc comments

--- packages/types/types/alternative-function.d.ts
+++ packages/types/types/alternative-function.d.ts
@@ -1,7 +1,17 @@
 /**
  * Alternative of `Function`
  *
- * {@link https://www.typescriptlang.org/docs/handbook/2/functions.html#function}
+ * Ban-types {@link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/ban-types.md#default-options}
+ *
+ * ## `Function` vs `(...args: Array<any>) => any`
+ *
+ * | `values`                        | `Function` | `() => any` | `(arg: any) => any` | `(arg1: any, arg2: any) => any` | `(...args: Array<any>) => any` |
+ * | :------------------------------ | :--------- | :---------- | :------------------ | :------------------------------ | :----------------------------- |
+ * | `Function`                      | ✅         | ❌          | ❌                  | ❌                              | ❌                             |
+ * | `() => any`                     | ✅         | ✅          | ✅                  | ✅                              | ✅                             |
+ * | `(arg: any) => any`             | ✅         | ❌          | ✅                  | ✅                              | ✅                             |
+ * | `(arg1: any, arg2: any) => any` | ✅         | ❌          | ❌                  | ✅                              | ✅                             |
+ * | `(...args: Array<any>) => any`  | ✅         | ✅          | ✅                  | ✅                              | ✅                             |
  *
  * @category Alternative
  */
