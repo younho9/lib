69a14cfff9bc10801e65c5755a4b643a99986c0a
Younho Choo <yo+github@younho9.com>
2021-12-10 22:38:17 +0900

    feat(`object`): add `defaultValue` argument to `get()`

--- packages/object/src/get.ts
+++ packages/object/src/get.ts
@@ -4,6 +4,7 @@ import type {Get} from 'type-fest';
 export function get<BaseType, Path extends string>(
   object: BaseType,
   path: Path,
+  defaultValue?: any,
 ): Get<BaseType, Path> {
-  return _get(object, path) as Get<BaseType, Path>;
+  return _get(object, path, defaultValue) as Get<BaseType, Path>;
 }
